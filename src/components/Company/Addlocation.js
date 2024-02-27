2959fe26774a0991a7918d466c4ef1612ecd2e0d
ashik nazeer <ashiknazeer@cordovacloud.com>
2024-02-28 04:42:30 +0530

    merge

---
+++ src/components/Company/Addlocation.js
@@ -0,0 +1,113 @@
+import { useState } from 'react';
+import React from 'react'
+import DrawerPop from '../common/DrawerPop'
+import { useTranslation } from 'react-i18next';
+import FormInput from '../common/FormInput';
+import TextArea from '../common/TextArea';
+import FlexCol from '../common/FlexCol';
+import { notification } from 'antd';
+
+const Location = ({open,
+    close = () => {},
+    updateId,
+    refresh = () => {},
+    companyDataId = "",} ) => {
+
+
+
+        const [show, setShow] = useState(open);
+        const { t } = useTranslation();
+        const [isUpdate, setIsUpdate] = useState();
+        const [api, contextHolder] = notification.useNotification();
+        const openNotification = (type, message, description, callback) => {
+          api[type]({
+            message: message,
+            description: description,
+            placement: "top",
+            onClose: callback,
+      
+            // stack: 2,
+            style: {
+              background: `${
+                type === "success"
+                  ? `linear-gradient(180deg, rgba(204, 255, 233, 0.8) 0%, rgba(235, 252, 248, 0.8) 51.08%, rgba(246, 251, 253, 0.8) 100%)`
+                  : "linear-gradient(180deg, rgba(255, 236, 236, 0.80) 0%, rgba(253, 246, 248, 0.80) 51.13%, rgba(251, 251, 254, 0.80) 100%)"
+              }`,
+              boxShadow: `${
+                type === "success"
+                  ? "0px 4.868px 11.358px rgba(62, 255, 93, 0.2)"
+                  : "0px 22px 60px rgba(134, 92, 144, 0.20)"
+              }`,
+            },
+            // duration: null,
+          });
+        };
+  return (
+    <DrawerPop
+    open={show}
+    close={(e) => {
+      // console.log(e);
+      close(e);
+    }}
+    contentWrapperStyle={{
+      maxWidth: "540px",
+    }}
+    handleSubmit={(e) => {
+      // console.log(e);
+    //   formik.handleSubmit();
+    }}
+    updateBtn={isUpdate}
+    updateFun={() => {
+    //   updateIdBasedLocation();
+    }}
+    header={[
+      !isUpdate ? t("Create Location") : t("Update_Location"),
+      !isUpdate
+        ? t("lorem ipusm")
+        : t("Update_Selected_Location"),
+    
+    ]}
+    footerBtn={[
+      t("Cancel"),
+      !isUpdate ? t("Add_Location") : t("Update_Location"),
+    ]}
+    
+    
+    >  
+          <FlexCol className="relative w-full h-full ">
+        <FormInput
+          title={t("Location")}
+          placeholder={t("Location")}
+        //   value={formik.values.location}
+          change={(e) => {
+            // formik.setFieldValue("location", e);
+          }}
+        //   error={formik.errors.location}
+        />
+
+        <TextArea
+          title={t("Description")}
+          placeholder={t("Enter_Description_here")}
+          className=""
+          change={(e) => {
+            // formik.setFieldValue("description", e);
+          }}
+        //   value={formik.values.description}
+        //   error={formik.errors.description}
+        />
+        {/* <ToggleBtn
+          title="Status"
+          titleRight="In Active"
+          change={(e) => {
+            formik.setFieldValue("isActive", e);
+          }}
+          value={formik.values.isActive}
+        /> */}
+        {contextHolder}
+      </FlexCol>
+        
+    </DrawerPop>
+  )
+}
+
+export default Location
--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -1,18 +1,24 @@
 import { title } from 'process'
 import Tabs from '../common/Tabs'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import TabsNew from '../common/TabsNew'
 import ButtonClick from '../common/Button'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
 import Departments from './Add _departments'
+import Location from './Addlocation'
+import API from '../Api'
+import axios from 'axios'
+import { setNavigationPath } from '../../Redux/action'
 
 
 const Company = () => {
     const { t } = useTranslation();
     const navigationPath = useSelector((state) => state.navigation.navigationPath);
+    const companySliceId = useSelector((state) => state.layout.companyId);
     const [show, setShow] = useState(false);
     const handleShow = () => setShow(true);
+    const dispatch = useDispatch();
   const tabs= [
     {
         id:1,
@@ -80,7 +86,66 @@ const Company = () => {
 }
 
   ]
+  const [locationList, setLocationList] = useState();
+  const [departmentList, setDepartmentList] = useState();
+  const actionData = [
+    {
+    //   company: { id: 1, data: companyList },
+    Locations: { id: 1, data: locationList },
+      Departments: { id: 2, data: departmentList },
+    //   category: { id: 4, data: categoryList },
+    //   subcategory: { id: 5, data: subCategoryList },
+    },
+  ];
+  const [companyId, setCompanyId] = useState(localStorage.getItem("companyId"));
+  
+  
+  const getLocationList = async () => {
+    console.log(companyId);
+    console.log(API.HOST + API.GET_LOCATION + "/" + companyId);
+    console.log("navigationPath");
+    const result = await axios.post(
+      API.HOST + API.GET_LOCATION + "/" + companyId
+    );
+    setLocationList(result.data.tbl_location);
+    console.log(result);
+  };
+  
+  const getDepartmentList = async () => {
+    console.log(API.HOST + API.GET_DEPARTMENT + "/" + companyId);
+    const result = await axios.post(
+      API.HOST + API.GET_DEPARTMENT + "/" + companyId
+    );
+    setDepartmentList(result.data.tbl_department);
+    console.log(result);
+  };
  
+  React.useEffect(() => {
+    // Provide a default value if needed
+    setCompanyId(companySliceId || localStorage.getItem("companyId"));
+    
+
+
+    switch (navigationPath) {
+      case "Locations":
+        getLocationList();
+        break;
+      case "Departments":
+        getDepartmentList();
+        break;
+      // Add more cases as needed
+      default:
+        break;
+    }
+
+    console.log(companySliceId, navigationPath, "refresh");
+  }, [companySliceId, dispatch, navigationPath]);
+
+  const tabClickHandler = (e) => {
+    console.log(e, "e");
+    // You can access navigationPath here and use it as needed
+    console.log("Navigation Path:", navigationPath);
+  };
     return (
     <div>
         <Tabs
@@ -92,6 +157,12 @@ const Company = () => {
         // console.log(e);
         // setShow(e);
       }}
+      tabClick={tabClickHandler}
+              data={
+          Object.keys(actionData[0]).includes(navigationPath)
+            ? actionData[0]?.[navigationPath].data
+            : null
+        }
        
         />
       
@@ -109,7 +180,19 @@ const Company = () => {
         />
       )}
 
-          
+{navigationPath === "Locations" && show && (
+        <Location
+          open={show}
+          close={(e) => {
+            setShow(e);
+          }}
+        //   updateId={updateId}
+        //   companyDataId={companyId}
+          refresh={() => {
+            // getLocationList();
+          }}
+        />
+      )}
             
     </div>
   )
