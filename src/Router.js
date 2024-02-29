aa169d98cea404e24df706703154c3bb7adab65e
AJAYSRAJENDRAN <ajayharigovindam@gmail.com>
2024-02-29 18:54:49 +0530

    push

--- src/Router.js
+++ src/Router.js
@@ -16,9 +16,7 @@ import JobTabs from "./components/common/JobTabs";
 import Notification from "./components/Settings/Notification";
 import Company from "./components/Company/Company";
 import TeamMembers from "./components/Team_members/TeamMembers";
-
-
-
+import Intergration from "./components/Settings/Intergration";
 
 export default function Router() {
   const { i18n } = useTranslation();
@@ -44,38 +42,37 @@ export default function Router() {
 
   return (
     <BrowserRouter basename="">
-        <div
-          className={`main_content flex bg-[#F8FAFC] dark:bg-black h-full min-h-screen font-Inter ${mode}`}
-          dir={layout}
-        >
-          {/* <NavigationMenu /> */}
-          
-          <Sidebar />
-          <div className="absolute top-0 h-full overflow-auto transition-all duration-300 home dark:bg-black pink:bg-pink-600">
-            <Header />
-            <div className="relative px-4 py-8 md:p-4 2xl:p-8 content">
-              <Routes>
-                {/* Company submenus */}
-                <Route path="/JobsList" element={<JobsList />} />
-                <Route path="/Appearance" element={<Appearance />} />
-                <Route path="/JobDetails" element={<JobDetails />} />
-                <Route path="/CandidateList" element={<CandidatesList />} />
-                <Route path="/CandidateProfile" element={<CandidateProfile />} />
-                <Route path="/AllJobs" element={<AllJobs/>} />
-                <Route path="/jobcard" element={<JobCard/>}></Route>
-                <Route path="/jobtabs" element={<JobTabs/>}></Route>
-                <Route path="/Notification" element={<Notification/>}></Route>
-                <Route path="/Company" element={<Company/>}></Route>
-                <Route path="/members" element={<TeamMembers/>}></Route>
-                
-              </Routes>
-            </div>
-          </div>
-          <div className="absolute bottom-0 right-0 p-2 opacity-40">
-            <h1 className="text-xs 2xl:text-md">Developement Mode</h1>
+      <div
+        className={`main_content flex bg-[#F8FAFC] dark:bg-black h-full min-h-screen font-Inter ${mode}`}
+        dir={layout}
+      >
+        {/* <NavigationMenu /> */}
+
+        <Sidebar />
+        <div className="absolute top-0 h-full overflow-auto transition-all duration-300 home dark:bg-black pink:bg-pink-600">
+          <Header />
+          <div className="relative px-4 py-8 md:p-4 2xl:p-8 content">
+            <Routes>
+              {/* Company submenus */}
+              <Route path="/JobsList" element={<JobsList />} />
+              <Route path="/Appearance" element={<Appearance />} />
+              <Route path="/JobDetails" element={<JobDetails />} />
+              <Route path="/CandidateList" element={<CandidatesList />} />
+              <Route path="/CandidateProfile" element={<CandidateProfile />} />
+              <Route path="/AllJobs" element={<AllJobs />} />
+              <Route path="/jobcard" element={<JobCard />}></Route>
+              <Route path="/jobtabs" element={<JobTabs />}></Route>
+              <Route path="/Notification" element={<Notification />}></Route>
+              <Route path="/Company" element={<Company />}></Route>
+              <Route path="/members" element={<TeamMembers />}></Route>
+              <Route path="/Integrations" element={<Intergration />}></Route>
+            </Routes>
           </div>
         </div>
-      
+        <div className="absolute bottom-0 right-0 p-2 opacity-40">
+          <h1 className="text-xs 2xl:text-md">Developement Mode</h1>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
