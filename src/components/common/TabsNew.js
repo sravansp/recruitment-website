166ec13be802397e46e27d38df88b86b36108649
libin <libin@docme.cloud>
2024-04-04 17:38:16 +0530

    tabs new changes addedd

--- src/components/common/TabsNew.js
+++ src/components/common/TabsNew.js
@@ -31,15 +31,15 @@ const TabsNew = ({ tabs, onTabChange, initialTab }) => {
               <motion.div
                 layoutId="bubble"
                 className="absolute inset-0 z-10 rounded-md bg-accent"
-                transition={{ type: "spring", duration: 0.6 }}
+                // transition={{ type: "spring", duration: 0.6 }}
               ></motion.div>
             )}
             <span
               className={`${
                 activeTab === tab.id
-                  ? "relative z-20 text-white"
+                  ? "relative inset-0 z-20 text-white rounded-md bg-accent p-2"
                   : " text-black dark:text-white group-hover:text-primary"
-              } vhcenter gap-1`}
+              } `}
             >
               {tab.icon && tab.icon} {tab.title}
             </span>
