f66a5fceefe9f66b169586f4078a026b0ceac126
Ricks331 <ricksachu@gmail.com>
2024-04-17 10:15:14 +0530

    some change

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -50,18 +50,18 @@ const Company = () => {
           title: "Description",
           value: "description",
         },
-        //  {
-        //   id:3,
-        //   title:"status",
-        //   value:"status",
-        //   actionToggle:true,
-        //  },
-        //  {
-        //   id:4,
-        //   title:"actions",
-        //   value:"actions",
-        //   action:true,
-        //  },
+         {
+          id:3,
+          title:"status",
+          value:"status",
+          actionToggle:true,
+         },
+         {
+          id:4,
+          title:"actions",
+          value:"actions",
+          action:true,
+         },
       ],
       Locations: [
         {
@@ -74,19 +74,18 @@ const Company = () => {
           title: "Description",
           value: "description",
         },
-        //  {
-        //   id:3,
-        //   title:"status",
-        //   value:"status",
-        //   actionToggle:true,
-        //  },
-        //  {
-        //   id:4,
-        //   title:"actions",
-        //   value:"actions",
-        //   action:true,
-        //  },
-
+         {
+          id:3,
+          title:"status",
+          value:"status",
+          actionToggle:true,
+         },
+         {
+          id:4,
+          title:"actions",
+          value:"actions",
+          action:true,
+         },
       ]
 
 
