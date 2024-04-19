47fbe81b5be3f9d3cb0d4a8ed0f9fb1209a6e9f7
Sravan SP <sravansp92@gmail.com>
2024-04-19 14:01:54 +0530

    Changes in Candidate Profile

--- src/components/Candidates/CandidateProfile.js
+++ src/components/Candidates/CandidateProfile.js
@@ -512,7 +512,7 @@ const CandidateProfile = () => {
       ))}
     </Menu>
   );
-  
+  const priority = 3;
   return (
     <div className="flex flex-col gap-6">
       {contextHolder}
@@ -590,6 +590,10 @@ const CandidateProfile = () => {
                   .map((name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
                   .join(' ')}</h2>
                     <PiBookmarkSimpleFill className=" text-[#12B76A] text-base" />
+                    <div className="bg-[#6A4BFC]/10 rounded-full px-4 py-1 text-[#6A4BFC] text-xs 2xl:text-sm font-semibold border border-[#6A4BFC]/20">
+                   {/* {jobName ? jobName : "Choose Job"} */}
+                    {jobName}
+                  </div> 
                   </div>
 
                   <div className="inline-flex items-center justify-start gap-4">
@@ -625,10 +629,12 @@ const CandidateProfile = () => {
                       </a>
                     </Dropdown>
                   ) : null}
+                   {
+                    !jobName && <div className="bg-[#6A4BFC]/10 rounded-full px-4 py-1 text-[#6A4BFC] text-xs 2xl:text-sm font-semibold border border-[#6A4BFC]/20">
+                    {jobName ? jobName : "Choose Job"}
+                   </div> 
+                   }
 
-                  <div className="bg-[#FFE8E8] rounded-full px-4 py-1">
-                    {jobName}
-                  </div>
                 </div>
                 <div className="flex flex-col gap-3">
                   <Dropdown overlay={menu} trigger={["click"]}>
@@ -639,11 +645,68 @@ const CandidateProfile = () => {
                       </Space>
                     </a>
                   </Dropdown>
-                  <div className="bg-[#FFE8E8] rounded-full px-4 py-1">
-                    {selectedItemLabel}
+                  <div className="bg-[#A95959]/10 rounded-full px-4 py-1 text-[#A95959] text-xs 2xl:text-sm font-semibold border border-[#A95959]/20">
+                    {selectedItemLabel ? selectedItemLabel : "Choose Stage"}
                   </div>
                 </div>
 
+                <Divider type="vertical" className="hidden h-auto lg:block" />
+                <div className="flex flex-col gap-3">
+                  <Dropdown overlay={menu} trigger={["click"]}>
+                    <a className="pblack" onClick={(e) => e.preventDefault()}>
+                      <Space>
+                        Priority
+                        <DownOutlined />
+                      </Space>
+                    </a>
+                  </Dropdown>
+                  <div className="flex gap-2 items-center">
+                    <div className="size-3 flex justify-between items-baseline">
+                      <span
+                        className={`${
+                          priority === 1
+                            ? "bg-red-500 opacity-100"
+                            : priority === 2
+                            ? "bg-orange-500 opacity-100"
+                            : "bg-[#12B76A] opacity-100"
+                        } w-0.5 rounded-sm h-1`}
+                      ></span>
+                      <span
+                        className={`${
+                          priority === 1
+                            ? "bg-red-500 opacity-20"
+                            : priority === 2
+                            ? "bg-orange-500 opacity-100"
+                            : priority === 3 && "bg-[#12B76A] opacity-100"
+                        } w-0.5 rounded-sm h-2`}
+                      ></span>
+                      <span
+                        className={`${
+                          priority === 1
+                            ? "bg-red-500 opacity-20"
+                            : priority === 2
+                            ? "bg-orange-500 opacity-20"
+                            : priority === 3 && "bg-[#12B76A] opacity-100"
+                        } w-0.5 rounded-sm h-3`}
+                      ></span>
+                    </div>
+                    <p
+                      className={`pblack ${
+                        priority === 1
+                          ? "!text-red-500"
+                          : priority === 2
+                          ? "!text-orange-500"
+                          : "!text-[#12B76A]"
+                      }`}
+                    >
+                      {priority === 1
+                        ? "Low"
+                        : priority === 2
+                        ? "Medium"
+                        : "High"}
+                    </p>
+                  </div>
+                </div>
                 <Divider type="vertical" className="hidden h-auto lg:block" />
                 <div className="flex flex-col gap-3">
                   <p className="pblack">Rating</p>
--- src/components/common/TextEditor/TextEditor.js
+++ src/components/common/TextEditor/TextEditor.js
@@ -14,9 +14,10 @@ const TextEditor = ({
   changetoHtml = () => {},
   className,
   minheight = "250px",
+  height = "",
   placeholder = "",
   loader = false,
-  error = ""
+  error = "",
 }) => {
   const [editorState, setEditorState] = useState(() => {
     if (initialValue) {
@@ -50,14 +51,17 @@ const TextEditor = ({
       const rawContentState = convertToRaw(contentState);
       const plainText = rawContentState.blocks
         .map((block) => block.text)
-        .join('\n');
+        .join("\n");
       const htmlContent = stateToHTML(contentState);
 
       onChange(plainText);
       changetoHtml(htmlContent);
+      console.log(plainText);
+      // Ensure onChange is called with plainText, which is a string
     }
   };
-
+  console.log(initialValue);
+  
   const uploadImageCallBack = (file) => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -96,9 +100,20 @@ const TextEditor = ({
       setEditorState(newEditorState);
     }
   };
-
   return (
-    <div className={`relative p-4 border border-black rounded-md border-opacity-10 dark:border-secondaryDark mb-14 ${className} ${loader ? 'vhcenter' : ''}`} style={{ minHeight: `${minheight}` }}>
+    <div
+      className={`relative p-4 border border-black rounded-md border-opacity-10 dark:border-secondaryDark mb-14 ${className} ${
+        loader ? "vhcenter" : ""
+      }`}
+      style={{
+        ...(error && {
+          boxShadow:
+            "0px 0px 0px 4px #FEE4E2, 0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
+            border: "1px solid red",
+        }),
+        minHeight: `${minheight}`,
+      }}
+    >
       <div className="flex">
         <p
           className={`text-xs font-medium 2xl:text-sm dark:text-white ${className}`}
@@ -115,27 +130,34 @@ const TextEditor = ({
           editorState={editorState}
           onEditorStateChange={handleEditorChange}
           placeholder={placeholder}
-          onDrop={handleDroppedImage}
+          wrapperStyle={{ height: height }}
           toolbar={{
-            options: ['inline', 'fontSize', 'list', 'textAlign', 'image'],
+            options: ["inline", "fontSize", "list", "textAlign"],
             inline: {
-              options: ['bold', 'italic', 'underline', 'strikethrough'],
+              options: ["bold", "italic", "underline", "strikethrough"],
             },
             list: {
-              options: ['unordered', 'ordered', 'indent'],
+              options: ["unordered", "ordered", "indent"],
             },
             textAlign: {
-              options: ['left', 'center', 'right', 'justify'],
+              options: ["left", "center", "right", "justify"],
+
             },
             image: {
               uploadCallback: uploadImageCallBack,
               alt: { present: true, mandatory: false },
             },
           }}
-          toolbarStyle={{ position: 'absolute', bottom: '-60px', left: '0', right: '0' }}
-          toolbarClassName=' bg-black'
-          editorClassName='h-full'
+          toolbarStyle={{
+            position: "absolute",
+            bottom: "-60px",
+            left: "0",
+            right: "0",
+          }}
+          toolbarClassName=" bg-black"
+          editorClassName="h-full"
         />
+          
         
       )}
      
