09c39898f43f8360d678256a9b8a3d8506e8e68b
Ricks331 <ricksachu@gmail.com>
2024-04-19 15:52:29 +0530

    notification added

--- src/components/Template/Workflowstage.js
+++ src/components/Template/Workflowstage.js
@@ -171,8 +171,8 @@ const Workflowstage = ({ open = "", close = () => { }, inputshow = false, isUpda
     enableReinitialize: true,
     validateOnChange: false,
     validationSchema: Yup.object().shape({
-      workFlowName : Yup.string().required('WorkFlow Name is required '),
-      description :  Yup.string().required('Description  is required '),
+      workFlowName: Yup.string().required('WorkFlow Name is required '),
+      description: Yup.string().required('Description  is required '),
 
     }),
     onSubmit: async (values, { setSubmitting }) => {
@@ -245,8 +245,11 @@ const Workflowstage = ({ open = "", close = () => { }, inputshow = false, isUpda
                 refresh();
               }, 1500);
             } else if (response2.status === 500) {
-              openNotification("error", "error", response2.message.replace(/<br\/>/g, '\n'));
+              openNotification("error", "error", response2.message);
             }
+          }
+          else if (response.status === 500) {
+            openNotification("error", "Failed", response.message);
           }
         }
       } catch (error) {
