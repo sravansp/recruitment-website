cfc638d4fe61e5e79def8aceb3ba20580c70545e
Ricks331 <ricksachu@gmail.com>
2024-04-25 14:32:11 +0530

    small issue fixed

--- src/components/Template/createJobtemp.js
+++ src/components/Template/createJobtemp.js
@@ -110,7 +110,7 @@ const CreatejobTemp = ({
   const [workFlows, setWorkFlows] = useState([]);
   const [selectedWorkFlowId, setSelectedWorkFlowId] = useState("");
   const [selectedDivs, setSelectedDivs] = useState([]);
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState(null);
   const [JobDescriptionList, setJobDescriptionList] = useState([])
   const [decriptionId, setDecriptionId] = useState(null)
   const [Phone, setPhone] = useState(1)
@@ -898,7 +898,7 @@ const CreatejobTemp = ({
           formik.setFieldError('salaryCurrency', !formik.values.salaryCurrency ? 'Salary Currency is required' : '');
           formik.setFieldError('jobType', !formik.values.jobType ? 'JobType is required' : '');
           formik.setFieldError('education', !formik.values.education ? 'Education is required' : '');
-          formik.setFieldError('jobDescription', !content ? 'Description is required' : '')
+          formik.setFieldError('jobDescription', !content ? 'Description is required' : '');
           return; // Exit early if any field is empty
         }
         if (jobcodelength === 0) {
@@ -1457,7 +1457,7 @@ const CreatejobTemp = ({
                       <Card>
                         <p className="pb-2">Description</p>
                         <TextEditor
-                          placeholder={t("Enter Description ")}
+                          placeholder={t("Enter Description")}
                           hideBorder={true}
                           initialValue={content}
                           //  change={(e)=>{
