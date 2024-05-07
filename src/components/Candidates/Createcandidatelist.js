1f70912f8bb099be51eb8737f85c6a0b26695edd
Ricks331 <ricksachu@gmail.com>
2024-05-07 12:03:50 +0530

    bug fixed

--- src/components/Candidates/Createcandidatelist.js
+++ src/components/Candidates/Createcandidatelist.js
@@ -40,7 +40,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Createcandidatelist({ open = "", close = () => { }, fileUpdateId, refresh, ConfigurationAction, updateId = null, }) {
   const [show, setShow] = useState(open);
-  const [activeBtnValue, setActiveBtnValue] = useState("Work");//Review//Questions//Work//Personel//Educational
+  const [activeBtnValue, setActiveBtnValue] = useState("Personel");//Review//Questions//Work//Personel//Educational
   const [nextStep, setNextStep] = useState(0);
   const [applicableData, setApplicableData] = useState([]);
   const [isUpdate, setIsUpdate] = useState();
