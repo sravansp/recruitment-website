9a3809a4dbf38d8ed78bd0594a5ef1615d8ec488
Ricks331 <ricksachu@gmail.com>
2024-05-07 12:10:41 +0530

    added degree

--- src/components/Candidates/Createcandidatelist.js
+++ src/components/Candidates/Createcandidatelist.js
@@ -427,7 +427,7 @@ export default function Createcandidatelist({ open = "", close = () => { }, file
   });
 
 
-  const Degree = [{ id: 1, title: "Bachelors", value: "Bachelors" }, { id: 2, title: "other", value: "other" }
+  const Degree = [{ id: 1, title: "Bachelors", value: "Bachelors" },{ id: 1, title: "Masters", value: "Masters" }, { id: 3, title: "other", value: "other" }
   ];
   const scrollRef = useRef();
 
