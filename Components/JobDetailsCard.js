a289c4d142b6bd411c637f9e506ba62f93c7a961
Ricks331 <ricksachu@gmail.com>
2024-05-10 15:44:41 +0530

    more data added

--- Components/JobDetailsCard.js
+++ Components/JobDetailsCard.js
@@ -27,17 +27,17 @@ const JobDetailsCard = ({ selectedJob, jobDetailsAnimation }) => {
     openDrawer();
   };
   // const handleApply = () => {
-   
+
   //   if (selectedJob) {
   //     // Navigate to the apply page with the selected job ID
   //     window.location.href = `/Form?jobId=${selectedJob.jobId}`;
   //     // const result = selectedJob.jobId
-      
+
   //   }
   // };
-console.log(selectedJob);
+  console.log(selectedJob);
   return (
-    
+
     <motion.div
       // initial={{ opacity: 0, y: 20 }}
       // animate={jobDetailsAnimation}
@@ -52,7 +52,7 @@ console.log(selectedJob);
           <div className="flex items-center justify-between">
             <h1 className="h2">{selectedJob.jobTitle}</h1>
             {/* <Link href="/login"> */}
-              <ButtonClick buttonName="Apply" BtnType="primary"  handleSubmit={() => handleApply(selectedJob.jobId)}/>
+            <ButtonClick buttonName="Apply" BtnType="primary" handleSubmit={() => handleApply(selectedJob.jobId)} />
             {/* </Link> */}
           </div>
           <div className="flex flex-col gap-2.5 2xl:gap-4 Overview">
@@ -76,7 +76,7 @@ console.log(selectedJob);
                 <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
                   salary range
                 </p>
-                <p className="col-span-4 pblack">{selectedJob.salaryCurrency+" "}{selectedJob.salaryRangeFrom}-{selectedJob.salaryRangeTo}</p>
+                <p className="col-span-4 pblack">{selectedJob.salaryCurrency + " "}{selectedJob.salaryRangeFrom}-{selectedJob.salaryRangeTo}</p>
               </div>
               <div className="grid grid-cols-6">
                 <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
@@ -105,40 +105,52 @@ console.log(selectedJob);
               <h3 className="pblack">Other Details</h3>
             </div>
             {/* {selectedJob ( */}
-              <div className="p-2.5 flex flex-col gap-4" >
-                <div className="grid grid-cols-6">
-                  <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
-                    industry
-                  </p>
-                  <p className="col-span-5 pblack"></p>
-                </div>
-                <div className="grid grid-cols-6">
-                  <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
-                    type
-                  </p>
-                  <p className="col-span-4 pblack">{selectedJob.jobType}</p>
-                </div>
+            <div className="p-2.5 flex flex-col gap-4" >
+              <div className="grid grid-cols-6">
+                <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
+                  Education
+                </p>
+                <p className="col-span-4 pblack">{selectedJob.education}</p>
               </div>
+              <div className="grid grid-cols-6">
+                <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
+                  Status
+                </p>
+                <p className="col-span-5 pblack">{selectedJob.jobStatus}</p>
+              </div>
+              <div className="grid grid-cols-6">
+                <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
+                  Requirement
+                </p>
+                <p className="col-span-4 pblack">{selectedJob.requirementType}</p>
+              </div>
+              <div className="grid grid-cols-6">
+                <p className="!font-normal !text-opacity-50 capitalize pblack col-span-1">
+                  Location type
+                </p>
+                <p className="col-span-4 pblack">{selectedJob.workLocationType}</p>
+              </div>
+            </div>
             {/* )} */}
           </div>
           <Drawer
-        placement="right"
-        closable={false}
-        onClose={closeDrawer}
-        visible={drawerVisible}
-        width="100%" // Adjust the width as needed
-        height="100%"
-        wrapperBodyStyle={{ backgroundColor: "#F8FAFC" }}
-        
-        
-      >
-        
-        <Web closeDrawer={closeDrawer} selectedJobId={selectedJobIdForApply} selectedJob />
-      </Drawer>
+            placement="right"
+            closable={false}
+            onClose={closeDrawer}
+            visible={drawerVisible}
+            width="100%" // Adjust the width as needed
+            height="100%"
+            wrapperBodyStyle={{ backgroundColor: "#F8FAFC" }}
+
+
+          >
+
+            <Web closeDrawer={closeDrawer} selectedJobId={selectedJobIdForApply} selectedJob />
+          </Drawer>
         </div>
       )}
     </motion.div>
-    
+
   );
 };
 
