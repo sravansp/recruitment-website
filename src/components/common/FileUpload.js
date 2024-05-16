b4daaccf926c19c9d403696df85be667027bf077
Ricks331 <ricksachu@gmail.com>
2024-05-16 17:17:40 +0530

    file upload fixed

--- src/components/common/FileUpload.js
+++ src/components/common/FileUpload.js
@@ -14,6 +14,8 @@ export default function FileUpload({
   const allowedFileFormats = ["jpg", "png", "jpeg", "svg", "webp", "pdf", "doc", "docx", "pptx"];
   const fileFormatsString = allowedFileFormats.join(", ");
   const [selectedFile, setSelectedFile] = useState(null);
+  const [nameList, setNameList] = useState([]);
+  const [changeStatus, setChangeStatus] = useState(false);
 
   const props = {
     name: "file",
@@ -24,25 +26,43 @@ export default function FileUpload({
       const isAllowedFile = allowedFileFormats.includes(fileExtension);
       if (!isAllowedFile) {
         message.error(`${file.name} file format is not supported.`);
+        setChangeStatus(false);
         return false;
       }
-      else {
-        return isAllowedFile;
+      else if (nameList.includes(file.name)) {
+        message.error(`${file.name} file is already uploaded.`);
+        setChangeStatus(false);
+        return false;
+      } else {
+        message.success(`${file.name} file added successfully.`);
+        setNameList([...nameList, file.name]);
+        setChangeStatus(true);
+        return false;
       }
     },
   };
 
+  // const handleChange = (info) => {
+  //   const { status, originFileObj } = info.file;
+  //   if (status !== "uploading") {
+  //     console.log(info.fileList);
+  //     console.log(status, ":file upload status");
+  //     console.log(originFileObj);
+  //   }
+  //   // Ensure only one file is selected
+  //   if (originFileObj && changeStatus) {
+  //     setSelectedFile(originFileObj);
+  //     change(originFileObj);
+  //   }
+  // };
+
   const handleChange = (info) => {
-    const { status, originFileObj } = info.file;
-    if (status !== "uploading") {
-      console.log(info.fileList);
-      console.log(status, ":file upload status");
-      console.log(originFileObj);
-    }
+    const { file } = info;
+
     // Ensure only one file is selected
-    if (originFileObj) {
-      setSelectedFile(originFileObj);
-      change(originFileObj);
+    if (file && changeStatus) {
+      setSelectedFile(file);
+      change(file);
     }
   };
 
@@ -63,7 +83,7 @@ export default function FileUpload({
           <div className="flex gap-2">
             <AiOutlineCloudUpload className="text-3xl text-primary " />
             <div className="flex flex-col">
-              <h2 className="acco-subhead"> {t("Click to upload")}</h2>
+              <h2 className="acco-subhead"> {t("Click or drag files to upload")}</h2>
               <p className="para px-5">{t("Allowed formats")}: {fileFormatsString}</p>
             </div>
           </div>
