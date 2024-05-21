5dcf99fa08c82c04009e51d629b82cc3e66790f5
Ricks331 <ricksachu@gmail.com>
2024-05-21 11:05:09 +0530

    small changes for bug fixing

--- src/components/Discover/PieChart.js
+++ src/components/Discover/PieChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   PieChart,
@@ -18,31 +18,31 @@ const COLORS = ["#0e2535", "#9da4fe", "#7942c5", "#ecc4f9", "#cfd6e6"];
 
 const PieChartWithLegends = () => {
   const theme = useSelector((state) => state.layout.mode)
-  const[data,SetData]=useState([])
+  const [data, SetData] = useState([])
   const [companyId, setCompanyId] = useState(localStorage.getItem("companyId"));
 
   useEffect(() => {
     setCompanyId(localStorage.getItem("companyId"));
-    
+
   }, []);
 
-  const getCandidateSource= async ()=>{
-    try{
-      const response = await getDashboardCandidateSource({companyId:companyId})
+  const getCandidateSource = async () => {
+    try {
+      const response = await getDashboardCandidateSource({ companyId: companyId })
       // console.log(response)
       const formattedResult = Object.entries(response.result).map(([name, value]) => ({
         name: name.toUpperCase(), // Convert month to uppercase
         value: value  // Calculate the frequency (multiplying by 1.8 as an example)
-    }));
-    // console.log(formattedResult, "formattedResult")
-    SetData(formattedResult)
-    }catch(error){
+      }));
+      // console.log(formattedResult, "formattedResult")
+      SetData(formattedResult)
+    } catch (error) {
       // console.log(error)
     }
   }
-  useEffect(()=>{
+  useEffect(() => {
     getCandidateSource()
-  },[])
+  }, [])
   const getTotalSources = () => {
     let total = 0;
     data.forEach((item) => (total += item.value));
@@ -60,11 +60,13 @@ const PieChartWithLegends = () => {
     const { cx, cy } = viewBox;
     return (
       <text x={cx} y={cy} textAnchor="middle" dominantBaseline="middle">
-       <tspan fill={theme === "dark" ? "white" : "black"} fontSize="10" fontWeight="normal">Total Sources</tspan>
-<tspan x={cx} dy="20" fill={theme === "dark" ? "white" : "black"} fontSize="18" fontWeight="bold">{totalSources}</tspan>
+        <tspan fill={theme === "dark" ? "white" : "black"} fontSize="10" fontWeight="normal">Total Sources</tspan>
+        <tspan x={cx} dy="20" fill={theme === "dark" ? "white" : "black"} fontSize="18" fontWeight="bold">{totalSources}</tspan>
       </text>
     );
   };
+
+
   return (
     <ResponsiveContainer width="100%" height={220}>
       <PieChart margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
@@ -87,7 +89,7 @@ const PieChartWithLegends = () => {
               stroke="none"
             />
           ))}
-        <Label content={labelText} position="center" />
+          <Label content={labelText} position="center" />
 
         </Pie>
         <Tooltip />
