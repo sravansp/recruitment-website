97d7593c9af63987e6134ed7375e8b83ac9ad031
Ricks331 <ricksachu@gmail.com>
2024-05-21 14:47:52 +0530

    mandatory symbol added

--- src/components/Template/AddEmailtemplate.js
+++ src/components/Template/AddEmailtemplate.js
@@ -13,6 +13,7 @@ import image2 from "../../assets/images/emoji-sticker-line.svg";
 
 import { saveRecruitmentEmailTemplate, getRecruitmentEmailTemplateById, updateRecruitmentEmailTemplate } from "../Api1";
 import { Subject, SubscriptionsOutlined } from "@mui/icons-material";
+import { FaAsterisk } from "react-icons/fa";
 const Emailtemplate = ({
   open = "",
   close = () => { },
@@ -311,7 +312,10 @@ const Emailtemplate = ({
 
             </div>
             <div>
-              <p className="pb-2">Email</p>
+              <p className="flex">
+                <p className="pb-2">Email</p>
+                <FaAsterisk className="ml-1.5 text-[6px] text-rose-600" />
+              </p>
               <TextEditor
                 placeholder={"Start typing your email"}
                 initialValue={content}
