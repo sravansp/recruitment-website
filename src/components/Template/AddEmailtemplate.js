22947080a4d4a5983634b3dbd7011303428c1cea
Ricks331 <ricksachu@gmail.com>
2024-06-04 15:58:38 +0530

    commented

--- src/components/Template/AddEmailtemplate.js
+++ src/components/Template/AddEmailtemplate.js
@@ -354,12 +354,12 @@ const Emailtemplate = ({
 
               />
             </div>
-            <div class="relative max-w-[1070px]  w-full mx-auto h-[49.72px] bg-purple-50 rounded-lg">
+            {/* <div class="relative max-w-[1070px]  w-full mx-auto h-[49.72px] bg-purple-50 rounded-lg">
               <div className="flex justify-start items-center m-3 gap-3">
-                {/* <img src={image}></img>
-                  <img src={image2}></img> */}
+                <img src={image}></img>
+                  <img src={image2}></img>
               </div>
-            </div>
+            </div> */}
           </Accordion>
         </div>
       </DrawerPop>
