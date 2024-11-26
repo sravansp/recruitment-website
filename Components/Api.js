cb1bdf36fdee3e7131676e1be2fa03527b098082
vijeesh <vijeesh@cordovacloud.com>
2024-11-26 14:05:01 +0530

    latest-localStorage

--- Components/Api.js
+++ Components/Api.js
@@ -5,10 +5,10 @@ const apiUrl = "https://alpha-jobs-api.loyaltri.com/api/v1";
 const fileapi = "https://alpha-jobs-api.loyaltri.com/api/fileHandler";
 const companyid = "https://alpha-api.loyaltri.com/api/main";
 
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
 
-// const token =
-//   typeof window !== "undefined" ? localStorage.getItem("token") : null;
+const token =
+  typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
 const apiRequest = async (action, method, kwargs) => {
   try {
